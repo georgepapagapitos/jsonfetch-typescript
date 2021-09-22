66cd85ee7cee08cc3f420a800ee8ec9ce28fb73b
George Papagapitos <george.g.papagapitos@gmail.com>
2021-09-22 14:22:34 -0500

    add expected data types to logTodo function

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1["default"].get(url).then(function (response) {
     var todo = response.data;
-    var id = todo.id, title = todo.title, completed = todo.completed;
-    console.log("\n  The Todo with ID: " + id + "\n  Has a title of: " + title + "\n  Is it finished? " + completed + "\n  ");
+    var id = todo.id;
+    var title = todo.title;
+    var completed = todo.completed;
+    logTodo(id, completed, title);
 });
+var logTodo = function (id, title, completed) {
+    console.log("\n  The Todo with ID: " + id + "\n  Has a title of: " + title + "\n  Is it finished? " + completed + "\n  ");
+};
