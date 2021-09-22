44f52b21943c3ccdf8dec6639a0e523125a247c0
George Papagapitos <george.g.papagapitos@gmail.com>
2021-09-22 13:36:54 -0500

    set up Todo interface

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    var todo = response.data;
+    var id = todo.id, title = todo.title, completed = todo.completed;
+    console.log("\n  The Todo with ID: " + id + "\n  Has a title of: " + title + "\n  Is it finished? " + completed + "\n  ");
 });
